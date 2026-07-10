3d0b9c98b709be0596d4e56b4e506cc6155ad42a
ChandanaLD <chandanadevaraj9535@gmail.com>
2026-07-10 19:20:02 +0530

    fix2

--- daikin/src/pages/shipment/GoodsMovement/createMovement.jsx
+++ daikin/src/pages/shipment/GoodsMovement/createMovement.jsx
@@ -79,7 +79,7 @@ const trackingToForm = (t) => {
     transportMode: t.transportationMode || t.transportMode || 'By Road',
     ewayBillNo: t.ewayBillNo || '',
     ewayBillDate: ewayBillDateVal,
-    lrNum: t.lr || t.invoiceNum || '',
+    lrNum: t.lr || '', 
     transporterName: t.transporter || '',
     driverName: t.driverName || '',
     contactNumber: t.contact || '',
