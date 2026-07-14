aff99ca691fe0dbb115accfefc9e5e7223630e27
ChandanaLD <chandanadevaraj9535@gmail.com>
2026-07-14 18:59:11 +0530

    deployment config fix

--- daikin/vite.config.js
+++ daikin/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   base: '/',
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: 'approuter/resources',
+    outDir: 'dist',
     emptyOutDir: true,
   }
 })
